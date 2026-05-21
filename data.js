2d360268b14f5b7959e63dcfa13d36c12b4b7ab5
ws20252026 <pinglass0511@gmail.com>
2026-05-21 17:10:25 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,21 @@
 export const dataPool = [
- 
   //公務員赴大陸//
   {
-    fortune: "公務員赴大陸-強化措施", //公務員赴大陸-事前申請
+    fortune: "公務員赴大陸-強化措施",
     yi: "事前申請",
     ji: "隱匿行程",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%85%AC%E5%8B%99%E5%93%A1%E8%B5%B4%E5%A4%A7%E9%99%B8-%E4%BA%8B%E5%89%8D%E7%94%B3%E8%AB%8B.png?raw=true",
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/公務員赴大陸-事前申請.png",
     tip: "赴陸交流務必依規事先申請，返台後如實通報。",
   },
-{
-    fortune: "公務員赴大陸-會見特定人員應記錄",//公務員赴大陸-會見特定人士
+  {
+    fortune: "公務員赴大陸-會見特定人員應記錄",
     yi: "落實登錄",
     ji: "私自接觸",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%85%AC%E5%8B%99%E5%93%A1%E8%B5%B4%E5%A4%A7%E9%99%B8-%E6%9C%83%E8%A6%8B%E7%89%B9%E5%AE%9A%E4%BA%BA%E5%A3%AB.png?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/公務員赴大陸-會見特定人士.png",
     tip: "與特定人員接觸應落實記錄。透明的行程，是公務生涯最好的保險。",
   },
 
-
-
- 
-
-/*{
+  /*{
     fortune: "赴陸異常應通報",
     yi: "行前報備；攜帶空白、沒有敏感機密資料的手機",
     ji: "隱蔽不報",
@@ -28,172 +23,129 @@ export const dataPool = [
     tip: "與特定人員接觸應落實記錄。透明的行程，是公務生涯最好的保險。",
   },*/
 
-
-
-// --- 主題 2：公務機密 (4組) ---//
+  // --- 主題 2：公務機密 (4組) ---//
   {
-    
-    fortune: "公務機密-守護者之盾", //個資洩密
+    fortune: "公務機密-守護者之盾",
     yi: "正當查詢",
     ji: "外流個資",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%85%AC%E5%8B%99%E6%A9%9F%E5%AF%86-%E5%80%8B%E8%B3%87%E6%B4%A9%E5%AF%86.png?raw=true", //個資洩密
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/公務機密-個資洩密.png",
     tip: "公務系統帳號僅限公務使用，嚴禁私用或外洩資料",
   },
   {
-    fortune: "公務機密-採購公正官靈籤",//公務機密-保密之廠商投標文件案
+    fortune: "公務機密-採購公正官靈籤",
     yi: "嚴守職守",
     ji: "洩漏底價",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%85%AC%E5%8B%99%E6%A9%9F%E5%AF%86-%E4%BF%9D%E5%AF%86%E4%B9%8B%E5%BB%A0%E5%95%86%E6%8A%95%E6%A8%99%E6%96%87%E4%BB%B6%E6%A1%88.png?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/公務機密-保密之廠商投標文件案.png",
     tip: "採購底價是不可觸碰的紅線。一時「熱心」告知，可能換來法律追訴。",
   },
   {
- 
-    fortune: "公務機密-資料外洩",//公務機密-拍照上傳
+    fortune: "公務機密-資料外洩",
     yi: "內斂修煉",
     ji: "社群打卡",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%85%AC%E5%8B%99%E6%A9%9F%E5%AF%86-%E6%8B%8D%E7%85%A7%E4%B8%8A%E5%82%B3.png?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/公務機密-拍照上傳.png",
     tip: "處理機密公務時，切勿拍照上傳社群網站。魔鬼藏在細節裡，洩密常在指尖發生。",
   },
   {
-    fortune: "公務機密-數位防線智慧錦囊",//公務機密-密碼貼桌上
+    fortune: "公務機密-數位防線智慧錦囊",
     yi: "定期更換",
     ji: "便箋貼桌",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%85%AC%E5%8B%99%E6%A9%9F%E5%AF%86-%E5%AF%86%E7%A2%BC%E8%B2%BC%E6%A1%8C%E4%B8%8A.png?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/公務機密-密碼貼桌上.png",
     tip: "電腦密碼不外流，切勿將密碼張貼於桌面或電腦周圍。守護資訊安全，就是守護自己的職涯。",
   },
 
-
- 
-// --- 主題 3：反詐騙 (4組) ---//
+  // --- 主題 3：反詐騙 (4組) ---//
   {
-    fortune: "反詐騙-用AI騙你錢",//反詐騙-AI騙錢
+    fortune: "反詐騙-用AI騙你錢",
     yi: "多方查證",
     ji: "急迫匯款",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%8F%8D%E8%A9%90%E9%A8%99-AI%E9%A8%99%E9%8C%A2.png?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/反詐騙-AI騙錢.png",
     tip: "接到『親朋好友』要錢？可能是AI!仔細看有無不合理（例如臉部模糊、不自然）（圖源：文山第二分局偵查隊 警務員周洛安）。",
   },
   {
-   
-    fortune: "反詐騙-假文字真詐財",//反詐騙-假補助
+    fortune: "反詐騙-假文字真詐財",
     yi: "小心確認",
     ji: "點擊簡訊",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%8F%8D%E8%A9%90%E9%A8%99-%E5%81%87%E8%A3%9C%E5%8A%A9.gif?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/反詐騙-假補助.gif",
     tip: "收到補助的電郵、簡訊，帳單的催繳，至公務機關官方網站、查證平台/組織查證。（圖源：文山第二分局偵查隊 警務員周洛安）。",
   },
   {
-  
-    fortune: "防詐明辨大師",//反詐騙-網路交友
+    fortune: "防詐明辨大師",
     yi: "感情可以談",
     ji: "眼見為憑",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%8F%8D%E8%A9%90%E9%A8%99-%E7%B6%B2%E8%B7%AF%E4%BA%A4%E5%8F%8B.png?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/反詐騙-網路交友.png",
     tip: "網路世界真假難辨，不被溫柔攻勢沖昏頭。保持清醒，上網查證。（圖源：文山第二分局偵查隊 警務員周洛安）",
   },
   {
-   
-    fortune: "不動如山清廉心",//反詐騙-投資詐騙
+    fortune: "不動如山清廉心",
     yi: "防詐警覺",
     ji: "貪圖利息",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%8F%8D%E8%A9%90%E9%A8%99-%E6%8A%95%E8%B3%87%E8%A9%90%E9%A8%99.png?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/反詐騙-投資詐騙.png",
     tip: "高獲利、零風險的投資多是騙局。保持清廉心境，貪念不動，騙術不靈。",
   },
 
- // --- 主題 4：消費者保護法 (2組) ---
+  // --- 主題 4：消費者保護法 (2組) ---
   {
-    fortune: "消費者保護法-正義守護卡",  //衝動消費
+    fortune: "消費者保護法-正義守護卡",
     yi: "審閱契約",
     ji: "衝動消費",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E8%A1%9D%E5%8B%95%E6%B6%88%E8%B2%BB.jpg?raw=true",  
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/衝動消費.jpg",
     tip: "職場生存要誠信，個人消費也要明智。",
   },
   {
-    
-    fortune: "消費者保護-聰明消費智慧籤", //避免誤入詐騙網站+APP小常識
+    fortune: "消費者保護-聰明消費智慧籤",
     yi: "官方商店下載",
     ji: "點擊不明連結",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E9%81%BF%E5%85%8D%E8%AA%A4%E5%85%A5%E8%A9%90%E9%A8%99%E7%B6%B2%E7%AB%99+APP%E5%B0%8F%E5%B8%B8%E8%AD%98.jpg?raw=true", 
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/避免誤入詐騙網站+APP小常識.jpg",
     tip: "進入網站前請認明官方網址，讓詐騙集團無機可乘。",
   },
- 
- // --- 主題 5：透明晶質獎 (1組) ---
+
+  // --- 主題 5：透明晶質獎 (1組) ---
   {
-   
-    fortune: "透明晶質獎",//法務部透明晶質獎簡介
-    yi: "廉政良善治理",
-    yi: "樹立標竿學習楷模",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E9%80%8F%E6%98%8E%E6%99%B6%E8%B3%AA%E7%8D%8E%E7%B0%A1%E4%BB%8B.jpg?raw=true", 
+    fortune: "透明晶質獎",
+    yi: "廉政良善治理、樹立標竿學習楷模",
+    ji: "無",
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/透明晶質獎簡介.jpg",
     tip: "行政透明是廉能政府的基石。讓陽光照進公務流程，激勵追求更好的廉能治理。",
   },
- 
-//---陽光法案---//
- 
- {
-    
-    fortune: "陽光法案-職權清廉警示碑", //反貪-利衝迴避
+
+  //---陽光法案---//
+  {
+    fortune: "陽光法案-職權清廉警示碑",
     yi: "利衝迴避",
     ji: "濫用圖利",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%8F%8D%E8%B2%AA-%E5%88%A9%E8%A1%9D%E8%BF%B4%E9%81%BF.png?raw=true",
-    tip: "公職人員於執行職務過程中，擁有相關權力或裁量權，因此容易遇到有圖利自己或他人機會。案例中民代以其身分兼廠商代表參加工程複驗，使女婿的「友門神土木包工業」通過驗收，未恪遵迴避規定並使其獲得財產上利益，而遭到裁罰。民意代表依法監督各級行政機關預算及施政作為，而行政機關即係受民代監督之機關，本案民代知道「友門神土木包工業」為其關係人，亦知其與鄉公所及所屬官員具職務監督關係，並對官員有影響力。更明白其以民意代表身分兼廠商代表參與系爭工程複驗，足以影響人民對公職人員廉潔操守及政府決策過程之信頼，仍決意在驗收紀錄上簽名，以圖「友門神土木包工業」財產上利益,實已違反本法第12條規定。",
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/反貪-利衝迴避.png",
+    tip: "公職人員於執行職務過程中，擁有相關權力或裁量權，因此容易遇到有圖利自己或他人機會。案例中民代以其身分兼廠商代表參加工程複驗，使女婿的「友門神土木包工業」通過驗收，未恪遵迴避規定並使其獲得財產上利益，而遭到裁罰。...",
   },
-
- {
-   
-    fortune: "陽光法案-防範利衝大師", //陽光-宮廟迴避
+  {
+    fortune: "陽光法案-防範利衝大師",
     yi: "依法公告",
     ji: "私相補助",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E9%99%BD%E5%85%89%E6%B3%95%E6%A1%88-%E5%AE%AE%E5%BB%9F%E8%BF%B4%E9%81%BF.png?raw=true", 
-    tip: "海派可以，違法不行！公職人員身兼民間團體（如宮廟）負責人時，該團體即屬於利衝法的「關係人」。機關若要給予補助，必須經過公開的公告程序，絕不能直接私下撥款。公私要分明，補助要公開，以免利衝法重罰上門。"
-},
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/陽光法案-宮廟迴避.png",
+    tip: "海派可以，違法不行！公職人員身兼民間團體（如宮廟）負責人時，該團體即屬於利衝法的「關係人」。...",
+  },
 
-
- //---反貪--//
+  //---反貪--//
   {
-    fortune: "反貪宣導不動如山清廉心",//反貪-工程監造與不法報酬
+    fortune: "反貪宣導不動如山清廉心",
     yi: "依法行政",
     ji: "接受請託",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%8F%8D%E8%B2%AA-%E5%B7%A5%E7%A8%8B%E7%9B%A3%E9%80%A0%E8%88%87%E4%B8%8D%E6%B3%95%E5%A0%B1%E9%85%AC.png?raw=true", 
-    tip: "嚴禁利用職務，貪圖不法利益。更不可假借公務刁難而主動索賄。"
-},
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/反貪-工程監造與不法報酬.png",
+    tip: "嚴禁利用職務，貪圖不法利益。更不可假借公務刁難而主動索賄。",
+  },
 
-//---公益揭弊者--//
-
-
-
-
- 
-//--- 主題 ：反賄選 (2組) ---
-{
-    fortune: "反賄選-民主基石守護卡",//反賄選海報
-    yi: "撥打檢舉賄選專線0800-024-099",
-    ji: "收受賄絡", 
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E6%B3%95%E5%8B%99%E9%83%A8_%E5%8F%8D%E8%B3%84%E9%81%B8%E6%B5%B7%E5%A0%B1_a_a1.jpg?raw=true", 
-    tip: "不會選才是真賄選"
-},
+  //--- 主題 ：反賄選 (2組) ---
   {
-    fortune: "反賄選-乾淨政治導航員", //反賄選-旅遊招待
+    fortune: "反賄選-民主基石守護卡",
+    yi: "撥打檢舉賄選專線0800-024-099",
+    ji: "收受賄絡",
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/法務部_反賄選海報_a_a1.jpg",
+    tip: "不會選才是真賄選",
+  },
+  {
+    fortune: "反賄選-乾淨政治導航員",
     yi: "乾淨選風",
     ji: "旅遊招待",
-    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%8F%8D%E8%B3%84%E9%81%B8-%E6%97%85%E9%81%8A%E6%8B%9B%E5%BE%85.png?raw=true", 
-    tip: "天下沒有免費的午餐，更沒有免費的旅遊！選舉期間，候選人以「免車資、包便當」等名義招待選民出遊，並在行程中進行競選宣傳、請求投票支持，已涉嫌觸犯《公職人員選舉罷免法》之賄選罪。拒絕不正利益，守護乾淨選風，才是真正的民主價值。"
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+    comic: "https://github.com/ws20252026/drawlots/raw/main/image/反賄選-旅遊招待.png",
+    tip: "天下沒有免費的午餐，更沒有免費的旅遊！選舉期間，候選人以「免車資、包便當」等名義招待選民出遊，並在行程中進行競選宣傳、請求投票支持，已涉嫌觸犯《公職人員選舉罷免法》之賄選罪。拒絕不正利益，守護乾淨選風，才是真正的民主價值。",
+  },
 ];
