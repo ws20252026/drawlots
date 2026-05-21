e375cf4e668bba45369771d475a8d10cfd5eb052
ws20252026 <pinglass0511@gmail.com>
2026-05-21 14:19:34 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,62 @@
 export const dataPool = [
+ 
+  //公務員赴大陸//
   {
-    fortune: "公務人員赴陸強化",
+    fortune: "公務員赴大陸-強化措施",
     yi: "事前申請",
     ji: "隱匿行程",
     comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%85%AC%E5%8B%99%E5%93%A1%E8%B5%B4%E5%A4%A7%E9%99%B8-%E4%BA%8B%E5%89%8D%E7%94%B3%E8%AB%8B.png?raw=true",
     tip: "赴陸交流務必依規事先申請，返台後如實通報。",
   },
+{
+    
+    fortune: "公務員赴大陸-會見特定人員應記錄",
+    yi: "落實登錄",
+    ji: "私自接觸",
+    comic: "comic_case_01.jpg", //公務員赴大陸-會見特定人士
+    tip: "與特定人員接觸應落實記錄。透明的行程，是公務生涯最好的保險。",
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 ];
