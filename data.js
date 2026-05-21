b0c5574c0bf00ecf97184073d046d563806ce8fa
ws20252026 <pinglass0511@gmail.com>
2026-05-21 12:34:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export const dataPool = [
     fortune: "公務人員赴陸強化",
     yi: "事前申請",
     ji: "隱匿行程",
-    comic: "image/公務員赴大陸-事前申請.png",
+    comic: "https://github.com/ws20252026/drawlots/blob/main/image/%E5%85%AC%E5%8B%99%E5%93%A1%E8%B5%B4%E5%A4%A7%E9%99%B8-%E4%BA%8B%E5%89%8D%E7%94%B3%E8%AB%8B.png?raw=true",
     tip: "赴陸交流務必依規事先申請，返台後如實通報。",
   },
 ];
